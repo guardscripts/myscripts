8ad1c69e5a3119510dfc4af7993ae10389012657
guard <80070502+guardscripts@users.noreply.github.com>
2021-07-08 18:21:57 -0700

    Create guardwalk.lua

---
+++ guardwalk.lua
@@ -0,0 +1,6 @@
+--first open source guard project yay fun moment 69
+
+while wait()do game:GetService("Players").LocalPlayer.Character.Animate.Disabled=true;
+    game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed=25;
+    game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower=2.48;
+game.Players.LocalPlayer.Character:findFirstChildOfClass("Humanoid"):ChangeState(4)end
